c56cb6fe3e4f77691aee663e775d3bb885882f12
ShifaaKhalil98 <shifaakhalil50@gmail.com>
2023-03-13 19:08:08 +0200

    fixed email and password validation

--- index.php
+++ index.php
@@ -3,17 +3,27 @@
 
     $email = $_POST['email'];
     $pwd = $_POST['pwd'];
-    $first_name = $_POST['confirm_pwd'];
+    $confirm_pwd = $_POST['confirm_pwd'];
     $year = $_POST['year'];
-
-        if(strlen($pwd) >= 8 && preg_match('/[A-Z]/', $pwd) && preg_match('/\d/', $pwd) && preg_match('/[!@#$%^&*()\-_=+{};:,<.>]/', $pwd)
+    
+    $response=[];
+    
+    // $pattern = '/^[^@\s]+@[^@\s]+\.[^@\s]+$/';
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $response['status']= false;
+    }else{
+       if(strlen($pwd) >= 8 && preg_match('/[A-Z]/', $pwd) && preg_match('/\d/', $pwd) && preg_match('/[!@#$%^&*()\-_=+{};:,<.>]/', $pwd)
         && preg_match('/[a-z]/', $pwd)) {
-            $response['status'] = 'registered';
-            echo json_encode($response);
-
-        } else {
-            $response['status'] = 'password not validated';
-            echo json_encode($response);
+           
+            if($pwd==$confirm_pwd){
+                $response['status'] = true;
+            }else{
+                $response['status']=false;
+            }
         }
-
+        else {
+            $response['status'] = false;
+        }
+    }
+        echo json_encode($response);
 ?>
