f06f24c7bfbc109e2bb9586291f05dbc0641e631
ShifaaKhalil98 <shifaakhalil50@gmail.com>
2023-03-13 19:29:07 +0200

    fixed the sorting API

--- sort.php
+++ sort.php
@@ -1,23 +1,19 @@
 
 <?php
-    include('connection.php');
+  $input = $_GET['input'];
+  $array=explode(',', $input);
+  $len=count($array);
 
-    $input= $_GET['input'];
-    $array = array();
-    for($i=0;$i<10;$i++){
-        $array.append($input);
-    }
-  $len = $array.length;
-  for ($i = 0; $i < $len; $i++) {
-    $min = $array[0];
-    $index = 0;
-    for ($j = 0; $j < num.length; $j++) {
-      if ($array[$j] < $min) {
-        $min = $array[$j];
-        $index = $j;
+  for ($i = 0; $i < $len - 1; $i++) {
+    $min_index = $i;
+    for ($j = $i + 1; $j < $len; $j++) {
+      if ($array[$j] < $array[$min_index]) {
+        $min_index = $j;
       }
     }
-    $new_array.append($min);
-}
-echo json_encode($new_array);
+    $temp = $array[$i];
+    $array[$i] = $array[$min_index];
+    $array[$min_index] = $temp;
+  }
+  echo json_encode($array);
 ?>
